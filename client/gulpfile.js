325e9cd447df77efa20be63899c10c50607cc202
amir-hos <amerhos799@gmail.com>
2022-10-11 14:50:36 +0200

    header-comp

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,3 +1,7 @@
 const gulp = require('gulp');
 const gulpSass = require('gulp-sass');
 const sass = gulpSass(require('sass'));
+
+gulp.task('sass' , async function(){
+    gulp.src('src/component/**/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
+})
---
+++ client/src/component/Header/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../css/Header/header.css";
+function Header(){
+    return(
+        <header>
+            shopping cart
+        </header>
+    )
+}
+export default Header;
