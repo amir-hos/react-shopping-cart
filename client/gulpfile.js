0768ae5ba41e17bec0ff0d6decf2a01f80be162d
amir-hos <amerhos799@gmail.com>
2022-10-11 16:01:33 +0200

    footer-comp

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -2,6 +2,12 @@ const gulp = require('gulp');
 const gulpSass = require('gulp-sass');
 const sass = gulpSass(require('sass'));
 
-gulp.task('sass' , async function(){
-    gulp.src('src/component/**/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
+//gulp.task('sass' , async function(){
+//   gulp.src('src/component/**/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
+//})
+
+gulp.task('watch' , async function(){
+    gulp.watch('src/component/**/*.scss' , async function(){
+        gulp.src('src/component/**/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
+    })
 })
---
+++ client/src/component/Footer/footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../css/Footer/footer.css";
+function Footer (){
+    return (
+        <footer>
+            footer
+        </footer>
+    )
+}
+export default Footer;
