a4cb3e1ba349e016111aed5e720ee81c82d79885
amir-hos <amerhos799@gmail.com>
2022-11-28 00:14:27 +0200

    shoppin-message

--- client/src/component/cart/cart.js
+++ client/src/component/cart/cart.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import '../../css/cart/cart.css';
 import CheckOutForm from '../checkOutForm/CheckOutForm';
 import Fade from 'react-reveal/Fade';
+import Modal from 'react-modal';
 import {connect} from 'react-redux';
 import {removeCart} from '../../store/actions/cart';
  function Cart(props) {
 
   const [checkForm , setCheckForm] = useState(false);
+  const [order , setOrder] = useState(false);
   const [value , setValue] = useState("")
 
   const handleSubmit = (e)=>{
@@ -15,10 +17,13 @@ import {removeCart} from '../../store/actions/cart';
       name : value.name ,
       email : value.email
     }
-    console.log(order);
+    setOrder(order);
   }
   const handleChange =(e)=>{
     setValue((prevState)=>({...prevState , [e.target.name] : [e.target.value]}))
+  }
+  const closeModal = () =>{
+    setOrder(false);
   }
   return (
     <div>
@@ -41,10 +46,50 @@ import {removeCart} from '../../store/actions/cart';
             ))}
         </div>
         </Fade>
+        {/* modal to show result of shopping  */}
+        <>
+        {order &&
+            <Modal isOpen={order} onRequestClose={closeModal}>
+                <div className="order-info">
+                    <span className="close-icon" onClick={closeModal}> &times; </span>
+                    <p className="alert-success"> order done success </p>
+                    <table>
+                        <tr>
+                            <td>Name:</td>
+                            <td>{order.name}</td>
+                        </tr>
+                        <tr>
+                            <td>Email:</td>
+                            <td>{order.email}</td>
+                        </tr>
+                        <tr>
+                            <td>Total:</td>
+                            <td>{props.cartItem.reduce( (a ,pr)=>{
+                              return a + (pr.price * pr.qty)
+                            } , 0)}</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Selected Items:
+                            </td>
+                            <td>{props.cartItem.map(p => (
+                                <div className="cart-data">
+                                    <p>Number of this products: {p.qty}</p>
+                                    <p>Title of  products: {p.title}</p>
+                                </div>
+                            ))}</td>
+                        </tr>
+                    </table>
+                </div>
+            </Modal> 
+        }
+        </>
+
+        
         {props.cartItem.length !== 0 && (
           <div className='tatal-cart'>
               <div className='total-price'>total: ${props.cartItem.reduce((acc , p)=>{
-                return acc + p.price
+                return (acc + (p.price * p.qty)) 
               } , 0)}</div>
               <button onClick={()=>setCheckForm(true)}>selectProduct</button>
         </div>
